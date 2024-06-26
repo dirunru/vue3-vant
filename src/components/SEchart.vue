dc6ca8968ead96cb14ad417af305926839af4396
dirunru <15514209607@163.com>
2024-06-26 15:38:34 +0800

    feat:echat盒子

--- src/components/SEchart.vue
+++ src/components/SEchart.vue
@@ -1,82 +1,57 @@
 <template>
-  <div ref="chartContainer" class="chart-container" :style="{ width: width, height: height }" />
+  <div ref="echartsRef" :style="{ width, height }"></div>
 </template>
 
-<script setup>
-  import { ref, onMounted, onBeforeUnmount, watch, defineExpose } from 'vue'
+<script>
+  import { onMounted, onUnmounted, ref, watch } from 'vue'
   import * as echarts from 'echarts'
 
-  const props = defineProps({
-    option: {
-      type: Object,
-      required: true
+  export default {
+    name: 'SEchart',
+    props: {
+      option: {
+        type: Object,
+        required: true
+      },
+      width: {
+        type: String,
+        default: '600px'
+      },
+      height: {
+        type: String,
+        default: '400px'
+      }
     },
-    width: {
-      type: String,
-      default: '100%'
-    },
-    height: {
-      type: String,
-      default: '100%'
-    }
-  })
+    setup(props) {
+      const echartsRef = ref(null)
+      let chartInstance = null
 
-  const chartContainer = ref(null)
-  let chartInstance = null
+      const initChart = () => {
+        chartInstance = echarts.init(echartsRef.value)
+        chartInstance.setOption(props.option)
+      }
 
-  onMounted(() => {
-    if (chartContainer.value) {
-      chartInstance = echarts.init(chartContainer.value)
-      updateChart()
-    }
-  })
+      onMounted(() => {
+        initChart()
+        window.addEventListener('resize', chartInstance.resize)
+      })
 
-  onBeforeUnmount(() => {
-    if (chartInstance) {
-      chartInstance.dispose()
-    }
-  })
+      onUnmounted(() => {
+        window.removeEventListener('resize', chartInstance.resize)
+        chartInstance.dispose()
+      })
 
-  watch(
-    () => props.option,
-    () => {
-      updateChart()
-    },
-    { deep: true }
-  )
+      watch(
+        () => props.option,
+        newOption => {
+          if (chartInstance) {
+            chartInstance.setOption(newOption)
+          }
+        },
+        { deep: true }
+      )
 
-  function updateChart() {
-    if (chartInstance) {
-      chartInstance.setOption(props.option, true)
+      return { echartsRef }
     }
   }
-
-  const resize = () => {
-    chartInstance && chartInstance.resize()
-  }
-
-  defineExpose({ resize })
 </script>
-<style scoped lang="less">
-  .chart-container {
-    ::v-deep div:nth-of-type(1) {
-      width: 100% !important;
-      height: 100% !important;
-
-      canvas {
-        width: 100% !important;
-        height: 100% !important;
-      }
-    }
-    ::v-deep div:nth-of-type(2) {
-      font-size: 12px !important;
-      pointer-events: auto !important;
-      div {
-        font-size: 12px !important;
-      }
-      span {
-        font-size: 12px !important;
-      }
-    }
-  }
-</style>
